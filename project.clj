1c812348dbaa34e2b59dcd2996993ac8037ad18f
Andres Gomez Urquiza <andres@fractal.ai>
2016-09-28 17:01:51 -0500

    updated deps

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-time "0.8.0"]
-                 [clojure-csv "2.0.1"]
-                 [http-kit "2.1.18"]
+                 [clj-time "0.12.0"]
+                 [clojure-csv "2.0.2"]
+                 [http-kit "2.2.0"]
                  [org.clojure/data.json "0.2.6"]
                  [com.outpace/clj-excel "0.0.9"]
                  [digitalize "0.1.0-SNAPSHOT"]
