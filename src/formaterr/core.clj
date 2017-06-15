9cdc20c1ddb9897e0aac7fd1377168ed0a9cf8ab
Andres Gomez Urquiza <andres.gomez@datos.mx>
2017-06-15 13:57:12 -0500

    download and copy fns

--- src/formaterr/core.clj
+++ src/formaterr/core.clj
@@ -5,7 +5,25 @@
             [clojure.string :as s]
             [clojure-csv.core :refer :all]
             [digitalize.core :refer :all]
-            [hozumi.det-enc :refer :all]))
+            [org.httpkit.client :as http]
+            [hozumi.det-enc :refer :all]
+            [clojure.java.io :as io]))
+
+
+(defn download
+  "Download a web resource"
+  [url]
+   (:body @(http/get url {:as :auto
+                          :insecure? true ; Needed to contact a server with an untrusted SSL cert
+                          :user-agent "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"})))
+
+(defn copy
+  "Copy a web resource to a file"
+  [url file]
+  (io/copy (:body @(http/get url {:as :stream
+                                  :insecure? true ; Needed to contact a server with an untrusted SSL cert
+                                  :user-agent "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"}))
+           (java.io.File. file)))
 
 (defn all-keys [rel]
   (distinct (flatten (map keys rel))))
@@ -124,8 +142,7 @@
 (defn json
   ([o]
    (if (string? o)
-     (json/read-str o
-                    :key-fn standard-keyword)
+     (json/read-str o :key-fn standard-keyword)
      (json/write-str o)))
   ([file o]
    (spit file (json o))))
