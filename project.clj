85b3949dc9a54a98f556cbfbc57f6b42a8f45c72
nex <andresgu@gmail.com>
2019-03-15 10:34:44 -0600

    updated deps

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/nez/formaterr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-time "0.14.2"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [clj-time "0.15.1"]
                  [clojure-csv "2.0.2"]
-                 [http-kit "2.3.0-alpha2"] ;[http-kit "2.2.0"]
+                 [http-kit "2.3.0"]
                  [org.clojure/data.json "0.2.6"]
                  [com.outpace/clj-excel "0.0.9"]
                  [digitalize "0.1.0-SNAPSHOT"]
-                 [org.clojars.hozumi/clj-det-enc "1.0.0-SNAPSHOT"]]
+                 [com.googlecode.juniversalchardet/juniversalchardet "1.0.3"]]
   :repl-options {:init-ns formaterr.core})
