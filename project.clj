7b25e2b993a40a214feff532157044a971b7daaa
Gu Andres <andresgu@gmail.com>
2020-07-29 18:59:11 -0500

    inicia 2.0.0

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject formaterr "1.0.0-SNAPSHOT"
+(defproject formaterr "2.0.0-SNAPSHOT"
   :description "Download, parse and write files in various formats"
   :url "https://github.com/nez/formaterr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [clj-http "3.9.1"]
-                 [clj-time "0.15.1"]
+                 [clj-http "3.10.1"]
+                 [clj-time "0.15.2"]
                  [clojure-csv "2.0.2"]
-                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/data.json "1.0.0"]
                  [com.outpace/clj-excel "0.0.9"]
                  [digitalize "0.1.1-SNAPSHOT"]
                  [org.clojars.smallrivers/juniversalchardet "1.0.3"]]
