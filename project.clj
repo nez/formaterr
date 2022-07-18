290e8d46faca5701843805a647f9622fbf8cc432
Gu Andres <andres@robotani.ca>
2022-07-18 16:59:26 -0500

    deps

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/nez/formaterr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [clj-http "3.10.1"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [clj-http "3.12.3"]
                  [clj-time "0.15.2"]
                  [clojure-csv "2.0.2"]
-                 [org.clojure/data.json "1.0.0"]
+                 [org.clojure/data.json "2.4.0"]
                  [com.outpace/clj-excel "0.0.9"]
                  [digitalize "0.1.1-SNAPSHOT"]
                  [org.clojars.smallrivers/juniversalchardet "1.0.3"]]
